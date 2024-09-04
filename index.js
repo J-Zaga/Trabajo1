443517f50095ec475e91460339b3da00afd33d89
J-Zaga <juan.zagarese@gmail.com>
2024-09-04 01:59:27 -0300

    Terminado

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hola Mundo!")
+const fs = require("fs")
+
+const datos = [2,10,"a",4,"b",6,"d",true,"e",9,1,"z",12,"r","c",false]
+const filtrar = (datos, condicion) => {
+    if(condicion != "" && condicion != null && condicion != undefined){
+     const filtrados = datos.filter((x) => typeof(x) == typeof(condicion))
+     filtrados.sort()
+     const unicos = [...new Set(filtrados)]
+     const datosParaEscribir = JSON.stringify(unicos, null, 2);
+     const writeFile = fs.writeFileSync("doc.txt", datosParaEscribir)
+     const updateFile = fs.readFileSync("doc.txt", "utf-8")
+     console.log(updateFile)
+}else {
+    return "La condicion no es valida"
+
+}
+}
+filtrar(datos,true)
