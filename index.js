698fb4a387fbfc89e9072693e52faa4a0ce4caf1
J-Zaga <juan.zagarese@gmail.com>
2024-09-04 02:28:26 -0300

    refinado

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const fs = require("fs")
 
-const datos = [2,10,"a",4,"b",6,"d",true,"e",9,1,"z",12,"r","c",false]
+const datos = [2, 10, "a", 4, "b", 6, "d", true, "e", 9, 1, "z", 12, "r", "c", false]
+
 const filtrar = (datos, condicion) => {
-    if(condicion != "" && condicion != null && condicion != undefined){
-     const filtrados = datos.filter((x) => typeof(x) == typeof(condicion))
-     filtrados.sort()
-     const unicos = [...new Set(filtrados)]
-     const datosParaEscribir = JSON.stringify(unicos, null, 2);
-     const writeFile = fs.writeFileSync("doc.txt", datosParaEscribir)
-     const updateFile = fs.readFileSync("doc.txt", "utf-8")
-     console.log(updateFile)
-}else {
-    return "La condicion no es valida"
+
+    if (typeof (condicion) === typeof ("string") || typeof (condicion) === typeof (1) || typeof (condicion) === typeof (true)) {
+        const filtrados = datos.filter((x) => typeof (x) == typeof (condicion))
+        filtrados.sort()
+        filtrados.sort((a, b) => a - b)
+        const unicos = [...new Set(filtrados)]
+        const datosParaEscribir = JSON.stringify(unicos, null, 2);
+        const writeFile = fs.writeFileSync("doc.txt", datosParaEscribir)
+        const updateFile = fs.readFileSync("doc.txt", "utf-8")
+        return updateFile
+    } else {
+        return "La condicion no es valida"
+    }
 
 }
-}
-filtrar(datos,true)
+
+console.log(filtrar(datos, 2))
